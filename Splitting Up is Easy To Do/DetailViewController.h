a5cb5355b5d2f497cb65b503ecfcb6bad1d11145
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-02-10 23:19:37 -0600

    Initial Commit

---
+++ Splitting Up is Easy To Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Splitting Up is Easy To Do
+//
+//  Created by XiaoYifan on 2/10/15.
+//  Copyright (c) 2015 XiaoYifan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Splitting Up is Easy To Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Splitting Up is Easy To Do
+//
+//  Created by XiaoYifan on 2/10/15.
+//  Copyright (c) 2015 XiaoYifan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
